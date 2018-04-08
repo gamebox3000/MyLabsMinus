99bae768483e9b74982e6c71d98fb9e5dd056b5e
Gamebox <gamebox3000@gmail.com>
2018-04-08 16:21:23 -0700

    addition and multipilication problems
    will work on grade scaling after all problem types are implimented

--- GroupProject/1020Group1Project/src/pkg1020group1project/ProblemGenerator.java
+++ GroupProject/1020Group1Project/src/pkg1020group1project/ProblemGenerator.java
@@ -15,25 +15,36 @@ public class ProblemGenerator {
     Random rand = new Random();
     double answer;
     String problem = "Please solve for x:\n";
-    
+    /**
+     * Verifies if an attempt is correct
+     * @param attempt double value to check if an answer is correct
+     * @return true if attempt is the same as answer
+     */
     public boolean verrifyAnswer(double attempt){
         return attempt == answer;     
     }
-    
+    /**
+     * Gets the answer
+     * @return the answer
+     */
     public double get_answer(){
         return answer;
     }
-    
+    /**
+     * Gets the problem string
+     * @return the problem string
+     */
     public String get_Problem(){
         return problem;
     }
     
     /**
-     * 
-     * @param type specifies either devi, multi, sub, or add
+     * This constructor specifies the type of math problem to create as well as what the grade level for the problem should be
+     * @param type specifies either devi, multi, sub, or add to control problem type
      * @param level specifies grade level
      */
     public ProblemGenerator(String type, double level){
+        level = Math.abs(level);
         switch (type){
             case "devi":
                 devi(level);
@@ -53,20 +64,44 @@ public class ProblemGenerator {
                 problem = "undefined problem";
         }         
     }
-    
+    /**
+     * creates a devision problem basses on a grade level by setting problem
+     * to the problem string and answer
+     * @param level grade level of the problem
+     */
     private void devi(double level){
         
     }
-    
+    /**
+     * creates a multiplication problem basses on a grade level by setting problem
+     * to the problem string and answer
+     * @param level grade level of the problem
+     */
     private void multi(double level){
-        
+        int numTerms = 1+(int)Math.ceil(level*rand.nextDouble());
+        int[] terms = new int[numTerms];
+        answer = 1;
+        for(int n = 0; n < terms.length; n++){
+            terms[n] = (int)(rand.nextDouble()*Math.pow(10, Math.ceil(level/3)));
+            answer = answer * terms[n];
+            if(n == terms.length-1){
+                problem += terms[n]+" = x";
+            } else{
+                problem += terms[n] +" * ";
+            }
+        }
     }
-    
+    /**
+     * creates a subtraction problem basses on a grade level by setting problem
+     * to the problem string and answer
+     * @param level grade level of the problem
+     */
     private void sub(double level){
         
     }
     /**
-     * creates an addition problem basses on a grade level
+     * creates an addition problem basses on a grade level by setting problem
+     * to the problem string and answer
      * @param level grade level of the problem
      */
     private void add(double level){
